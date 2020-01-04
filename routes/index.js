3795157c331cf1a4b1b4132d51b7930ab2e18074
sandrine <sandrinezocca@gmail.com>
2020-01-04 16:30:39 +0100

    creating views for tweets

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 // création du router
-const router = require('express').Router();
+const router = require("express").Router(); // <--- on appelle directement Router
 
 // définir une route
-router.get('/', (req, res) => {
-    res.render('home');
-})
+router.get("/", (req, res) => {
+  res.render("tweets/tweet-list");
+});
 
 // exporter le router
 module.exports = router;
