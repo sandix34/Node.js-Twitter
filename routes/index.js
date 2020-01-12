d3d9d9489e4d2315586504f8ffa2a972d22c4555
sandrine <sandrinezocca@gmail.com>
2020-01-12 13:31:43 +0100

    🐦  display the list of tweets

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 // création du router
 const router = require("express").Router(); // <--- on appelle directement Router
-const api = require('./api');
+const api = require("./api");
+const Tweet = require("../database/models/tweet.model");
 
 // connecter l'api
-router.use('/api', api);
+router.use("/api", api);
 
 // définir les routes
 router.get("/tweet/new", (req, res) => {
@@ -11,8 +12,10 @@ router.get("/tweet/new", (req, res) => {
 });
 
 router.get("/", (req, res) => {
-  res.render("tweets/tweet-list");
-});
+  Tweet.find({})
+      .exec()
+      .then( tweets => res.render("tweets/tweet-list", { tweets }))
+})
 
 // exporter le router
 module.exports = router;
