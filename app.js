81cfc19ad0a9ed08998addb70bab8a29db86a4b2
sandrine <sandrinezocca@gmail.com>
2020-01-13 16:37:34 +0100

    ➕  environments and error handling

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 // récupère le package Express
-const express = require('express');
+const express = require("express");
 // logger morgan
-const morgan = require('morgan');
+const morgan = require("morgan");
 // path
-const path = require('path');
+const path = require("path");
 // route index
-const index = require('./routes');
+const index = require("./routes");
 // base de donnée
-require('./database');
+require("./database");
+// gestionnaire d'erreurs de développement uniquement
+const errorHandler = require("errorhandler");
 
 // création de app à partir de la fonction express
 const app = express();
@@ -15,26 +17,34 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // définir le folder pour les vues
-app.set('views', path.join(__dirname, 'views') );
+app.set("views", path.join(__dirname, "views"));
 // spécifier le view engine pour ne pas avoir à taper l'extension à chaque utilisation
-app.set('view engine', 'pug');
+app.set("view engine", "pug");
 
 // ----- définir les midelwares -------
 
 // récupérer le logger morgan
-app.use(morgan('short'));
+app.use(morgan("short"));
 // récupérer des statiques
-app.use(express.static(path.join(__dirname, 'public') ));
+app.use(express.static(path.join(__dirname, "public")));
 // récupérer les datas via la méthode POST
 app.use(express.json());
 // récupérer la data sous un autre format que JSON
-app.use(express.urlencoded({extended : true}));
-
-// ------ définir les routes ----------
-
+app.use(express.urlencoded({ extended: true }));
 // point d'entrée des routes
 app.use(index);
 
+if (process.env.NODE_ENV === "development") {
+  app.use(errorHandler());
+} else {
+  app.use((err, req, res, next) => {
+    const code = err.code || 500
+    res.status(code).json({
+      code: code,
+      message: code === 500 ? null : err.message
+    })
+  })
+}
 
-// écouter un port 
+// écouter un port
 app.listen(port);
