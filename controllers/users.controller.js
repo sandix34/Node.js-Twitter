5b6ada6f0325de022f4b4228a6ee75184875cac8
sandrine <sandrinezocca@gmail.com>
2020-02-22 16:37:51 +0100

    ⚙️ implementation of controller logic

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,7 +1,17 @@
+const { createUser } = require('../queries/users.queries');
+
+// permet d'envoyer le formulaire d'inscription à l'utilisateur
 exports.signupForm = (req, res, next) => {
-  res.end();
+  res.render('users/user-form', { errors: null });
 }
 
+// permet de créer un nouvel utilisateur dans la base de donnée
 exports.signup = async (req, res, next) => {
-    res.end();
+  const body = req.body;
+  try {
+    const user = await createUser(body);
+    res.redirect('/');
+  } catch(e) {
+    res.render('users/user-form', { errors: [ e.message ] });
+  }
 }
